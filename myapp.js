0c5a8eeb8bdf8e5ad570e52effd5d11b3d1aa8c1
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-20 21:13:49 -0400

    organizing css

--- myapp.js
+++ myapp.js
@@ -1,6 +1,10 @@
 var pgp = require( 'pg-promise' )(/*options*/)
 
 var MYAPP = ( function() {
+
+    var trips = [];
+    var trip = null;
+
     /*
     -------------------------------------------------------------
     */
@@ -8,9 +12,7 @@ var MYAPP = ( function() {
     var db = pgp( 'postgres://postgres:oova@localhost:5432/camping' );
     // db = pgp( 'postgres://postgres:oova@localhost:5433/camping' );
     // db = pgp( 'postgres://lipotmujqxlpqp:942c5578a0c0cd60928ae78651b4134f9a74b859a06c3be8934fa2b9ef395c50@ec2-107-22-211-182.compute-1.amazonaws.com:5432/d232e3aq43o7fj' );
-    var trips = [];
-    var trip = null;
-
+    
     var sql = 'SELECT * FROM tbl_trips AS trips JOIN tbl_campgrounds AS camp ON trips.fld_campground_id::int = camp.id ORDER BY trips.id DESC'
 
     db.any( sql, [true] ).then( function( data ) {
@@ -51,7 +53,7 @@ var MYAPP = ( function() {
                 trip = item;
             }
         });
-        
+
         return trip;
     };
     /*
